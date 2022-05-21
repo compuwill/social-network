2f72bcf8c5905dddadd0d9a3e19cb2e1c385089b
compuwill <wpvorngsam@hotmail.com>
2022-05-20 20:44:19 -0500

    added user structure

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email:{
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    }
+)
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
