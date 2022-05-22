2ae679cf2a4ba58ff663e3f78df4cf04d501a596
compuwill <wpvorngsam@hotmail.com>
2022-05-21 22:28:45 -0500

    added reaction schema and util

--- models/thought.js
+++ models/thought.js
@@ -1,4 +1,30 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            trim: true
+        },
+        username:
+        {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+)
 
 const ThoughtSchema = new Schema(
     {
@@ -9,24 +35,35 @@ const ThoughtSchema = new Schema(
             maxlength: 280,
             trim: true
         },
-        createdAt:{
+        createdAt: {
             type: Date,
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
         },
         username: {
+            type: String,
+            required: true
+        },
+        reactions: [
+            {
                 type: Schema.Types.ObjectId,
-                ref: 'User'
+                ref: 'Reaction'
             }
-        
+        ]
+
     },
     {
         toJSON: {
             virtuals: true,
             getters: true
-          }
+        }
     }
 )
+
+// get total count of reaction and replies on retrieval
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
 
 // create the Thought model using the ThoughtSchema
 const Thought = model('Thought', ThoughtSchema);
